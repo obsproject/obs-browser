834edb94b0421e8943d0760c263e49bbc446b20c
Michel <michel@sokar.nl>
2016-08-07 16:39:43 +0200

    Set command-line arguments for best off-screen rendering performance This is also a workaround for the issue where CEF wouldn't render anything.

--- shared/browser-app.cpp
+++ shared/browser-app.cpp
@@ -38,4 +38,7 @@ void BrowserApp::OnBeforeCommandLineProcessing(
 	CefRefPtr<CefCommandLine> command_line)
 {
 	command_line->AppendSwitch("enable-system-flash");
+	command_line->AppendSwitch("disable-gpu");
+	command_line->AppendSwitch("disable-gpu-compositing");
+	command_line->AppendSwitch("enable-begin-frame-scheduling");
 }
