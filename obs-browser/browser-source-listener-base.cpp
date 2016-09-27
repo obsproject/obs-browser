2eaf69f7a3d2ac98a45d5cc96124ab7c666bfa2f
jp9000 <obs.jim@gmail.com>
2016-09-27 13:14:40 -0700

    Use TryLockTexture for OnDraw
    When the user updates the browser source settings and the browser shuts
    down (BrowserManager::Impl::DestroyBrowser) in the deferred update in
    the libobs graphics thread, the BrowserSource::lockTexture will be
    locked, and while it's locked, it'll post a shutdown task to the
    CrBrowserMain task queue thread, and wait for that task to be executed.
    However, at the same time, the task queue thread could randomly get
    stuck waiting to lock the texture in the
    BrowserSource::Impl::Listener::OnDraw function.  So the libobs graphics
    thread would be waiting for the task queue, but the task queue would be
    stuck waiting for the libobs graphics thread, causing a hard lock.
    
    The solution to this is to simply make the OnDraw function try to lock
    rather than wait to lock, and then ignore if the mutex is busy.  That
    way it'll stop trying to update frames while the browser is shutting
    down.  This shouldn't impact browser draw performance or prevent browser
    frames from being drawn because the only time when another thread is
    locking the texture is when the user updates the browser source.

--- obs-browser/browser-source-listener-base.cpp
+++ obs-browser/browser-source-listener-base.cpp
@@ -29,11 +29,12 @@ void BrowserSource::Impl::Listener::OnDraw( BrowserSurfaceHandle surfaceHandle,
 	if (textureSet.count(surfaceHandle) > 0) {
 
 		obs_enter_graphics();
-		browserSource->GetParent()->LockTexture();
-		if (popupSurface != nullptr)
-			gs_copy_texture_region(surfaceHandle, popupX, popupY, popupSurface, 0, 0, gs_texture_get_width(popupSurface), gs_texture_get_height(popupSurface));	
-		browserSource->SetActiveTexture(surfaceHandle);
-		browserSource->GetParent()->UnlockTexture();
+		if (browserSource->GetParent()->TryLockTexture()) {
+			if (popupSurface != nullptr)
+				gs_copy_texture_region(surfaceHandle, popupX, popupY, popupSurface, 0, 0, gs_texture_get_width(popupSurface), gs_texture_get_height(popupSurface));
+			browserSource->SetActiveTexture(surfaceHandle);
+			browserSource->GetParent()->UnlockTexture();
+		}
 		obs_leave_graphics();
 
 	}
