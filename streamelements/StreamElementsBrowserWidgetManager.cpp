fe60865116cf16954aaa079e00a79385b447c1f5
Ilya Melamed <ilya77@gmail.com>
2018-07-09 14:58:56 +0300

    Additional sync and ->deleteLater() instead of delete when removing notification bar

--- streamelements/StreamElementsBrowserWidgetManager.cpp
+++ streamelements/StreamElementsBrowserWidgetManager.cpp
@@ -339,7 +339,7 @@ void StreamElementsBrowserWidgetManager::HideNotificationBar()
 
 		mainWindow()->removeToolBar(m_notificationBarToolBar);
 
-		delete m_notificationBarToolBar;
+		m_notificationBarToolBar->deleteLater();
 
 		m_notificationBarToolBar = nullptr;
 		m_notificationBarBrowserWidget = nullptr;
