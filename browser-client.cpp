obsproject/obs-browser

commit 835b5418bfe77e5338a0322365011f0761bc1048

Fix building with CEF versions higher than or equal to 3578

browser-client.cpp
@@ -139,17 +139,20 @@ bool BrowserClient::OnProcessMessageReceived(
 
 	return true;
 }
-
+#if CHROME_VERSION_BUILD >= 3578
+void BrowserClient::GetViewRect(
+#else
 bool BrowserClient::GetViewRect(
+#endif
 		CefRefPtr<CefBrowser>,
 		CefRect &rect)
 {
 	if (!bs) {
-		return false;
+		return;
 	}
 
 	rect.Set(0, 0, bs->width, bs->height);
-	return true;
+	return;
 }
 
 void BrowserClient::OnPaint(

browser-client.hpp
@@ -98,7 +98,11 @@ public:
 			CefRefPtr<CefMenuModel> model) override;
 
 	/* CefRenderHandler */
+#if CHROME_VERSION_BUILD >= 3578
+	virtual void GetViewRect(
+#else
 	virtual bool GetViewRect(
+#endif
 			CefRefPtr<CefBrowser> browser,
 			CefRect &rect) override;
 	virtual void OnPaint(

commit 042bbde6aece158c399070b680b48cea7b170159

Fixed compilation with Chromium < 3578
Regression from 835b5418bfe77e5338a0322365011f0761bc1048

browser-client.cpp
@@ -148,11 +148,19 @@ bool BrowserClient::GetViewRect(
 		CefRect &rect)
 {
 	if (!bs) {
+#if CHROME_VERSION_BUILD >= 3578
 		return;
+#else
+		return false;
+#endif
 	}
 
 	rect.Set(0, 0, bs->width, bs->height);
+#if CHROME_VERSION_BUILD >= 3578
 	return;
+#else
+	return true;
+#endif
 }
 
 void BrowserClient::OnPaint(