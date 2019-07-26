ca0f7e6175006ed441a823cca74865b20cf72017
jp9000 <obs.jim@gmail.com>
2019-07-25 23:46:25 -0700

    Enable audio monitoring by default
    After merging the new audio capture code, browser sources will now
    unfortunately not play audio to desktop like they used to.  This changes
    it so that their default behavior is to enable audio monitoring by
    default, thus restoring their original behavior.  Audio monitoring can
    then be turned off by the user if they so choose.

--- obs-browser-plugin.cpp
+++ obs-browser-plugin.cpp
@@ -316,7 +316,8 @@ void RegisterBrowserSource()
 			    OBS_SOURCE_AUDIO |
 #endif
 			    OBS_SOURCE_CUSTOM_DRAW | OBS_SOURCE_INTERACTION |
-			    OBS_SOURCE_DO_NOT_DUPLICATE;
+			    OBS_SOURCE_DO_NOT_DUPLICATE |
+			    OBS_SOURCE_MONITOR_BY_DEFAULT;
 	info.get_properties = browser_source_get_properties;
 	info.get_defaults = browser_source_get_defaults;
 
