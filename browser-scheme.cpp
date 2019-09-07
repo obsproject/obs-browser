6c2eeff6ec7e4234a6ad92206f36370727dc0e3a
Ilya Melamed <ilya77@gmail.com>
2019-09-07 19:01:21 +0300

    obs-browser: enable large local media file access
    In newer CEF builds, the http://absolute/ scheme handler factory seems
    to never complete when accessing large media files due to an apparent
    bug in the Chromium media player (XHR requests complete fine).
    At the same time, file:// URLs are working just fine.
    
    file:// URLs will be used where available (CEF 3440+) falling back to
    `BrowserSchemeHandlerFactory` on older CEF versions.
    
    When file:// URLs are in effect, we'll disable `web_security` to allow
    CORS requests to remote APIs, preserving functionality previously
    enabled by a custom browser scheme handler.
    
    `BrowserSchemeHandlerFactory` is simplified by using CEF built in
    `CefStreamResourceHandler` in accord with `CefStreamReader` instead of
    manually implementing a `CefResourceHandler`.

--- browser-scheme.cpp
+++ browser-scheme.cpp
@@ -18,9 +18,9 @@
 
 #include "browser-scheme.hpp"
 #include "wide-string.hpp"
+#include "include/wrapper/cef_stream_resource_handler.h"
 
-/* ------------------------------------------------------------------------- */
-
+#if !ENABLE_LOCAL_FILE_URL_SCHEME
 CefRefPtr<CefResourceHandler>
 BrowserSchemeHandlerFactory::Create(CefRefPtr<CefBrowser> browser,
 				    CefRefPtr<CefFrame>, const CefString &,
@@ -29,14 +29,6 @@ BrowserSchemeHandlerFactory::Create(CefRefPtr<CefBrowser> browser,
 	if (!browser || !request)
 		return nullptr;
 
-	return CefRefPtr<BrowserSchemeHandler>(new BrowserSchemeHandler());
-}
-
-/* ------------------------------------------------------------------------- */
-
-bool BrowserSchemeHandler::ProcessRequest(CefRefPtr<CefRequest> request,
-					  CefRefPtr<CefCallback> callback)
-{
 	CefURLParts parts;
 	CefParseURL(request->GetURL(), parts);
 
@@ -48,79 +40,22 @@ bool BrowserSchemeHandler::ProcessRequest(CefRefPtr<CefRequest> request,
 		cef_uri_unescape_rule_t::
 			UU_URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS);
 
-#ifdef WIN32
-	inputStream.open(to_wide(path.erase(0, 1)), std::ifstream::binary);
-#else
-	inputStream.open(path, std::ifstream::binary);
-#endif
-
-	/* Set fileName for use in GetResponseHeaders */
-	fileName = path;
-
-	if (!inputStream.is_open()) {
-		return false;
-	}
-
-	inputStream.seekg(0, std::ifstream::end);
-	length = remaining = inputStream.tellg();
-	inputStream.seekg(0, std::ifstream::beg);
-	callback->Continue();
-	return true;
-}
-
-void BrowserSchemeHandler::GetResponseHeaders(CefRefPtr<CefResponse> response,
-					      int64 &response_length,
-					      CefString &redirectUrl)
-{
-	if (!response) {
-		response_length = -1;
-		redirectUrl = "";
-		return;
-	}
-
-	std::string fileExtension =
-		fileName.substr(fileName.find_last_of(".") + 1);
+	std::string fileExtension = path.substr(path.find_last_of(".") + 1);
 
 	for (char &ch : fileExtension)
 		ch = (char)tolower(ch);
 	if (fileExtension.compare("woff2") == 0)
 		fileExtension = "woff";
 
-	response->SetStatus(200);
-	response->SetMimeType(CefGetMimeType(fileExtension));
-	response->SetStatusText("OK");
-	response_length = length;
-	redirectUrl = "";
+#ifdef _WIN32
+	CefRefPtr<CefStreamReader> stream =
+		CefStreamReader::CreateForFile(path.substr(1));
+#else
+	CefRefPtr<CefStreamReader> stream =
+		CefStreamReader::CreateForFile(path);
+#endif
+
+	return new CefStreamResourceHandler(CefGetMimeType(fileExtension),
+					    stream);
 }
-
-bool BrowserSchemeHandler::ReadResponse(void *data_out, int bytes_to_read,
-					int &bytes_read, CefRefPtr<CefCallback>)
-{
-	if (!data_out || !inputStream.is_open()) {
-		bytes_read = 0;
-		inputStream.close();
-		return false;
-	}
-
-	if (isComplete) {
-		bytes_read = 0;
-		return false;
-	}
-
-	inputStream.read((char *)data_out, bytes_to_read);
-	bytes_read = (int)inputStream.gcount();
-	remaining -= bytes_read;
-
-	if (remaining == 0) {
-		isComplete = true;
-		inputStream.close();
-	}
-
-	return true;
-}
-
-void BrowserSchemeHandler::Cancel()
-{
-	if (inputStream.is_open())
-		inputStream.close();
-}
+#endif
