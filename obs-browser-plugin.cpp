853e59f7bd71cc1728c46da5158315fe3e3771ce
jp9000 <obs.jim@gmail.com>
2019-09-19 18:31:32 -0700

    Disable rerouting by default
    It was decided that due to the fact that audio monitoring has not had
    testing on this wide of a scale of users before, that holding off on
    rerouting audio is the best interim solution -- make it optional, and
    off by default.  Users can still use this, but they will need to
    explicitly enable it themselves.

--- obs-browser-plugin.cpp
+++ obs-browser-plugin.cpp
@@ -123,7 +123,7 @@ static void browser_source_get_defaults(obs_data_t *settings)
 	obs_data_set_default_bool(settings, "shutdown", false);
 	obs_data_set_default_bool(settings, "restart_when_active", false);
 	obs_data_set_default_string(settings, "css", default_css);
-	obs_data_set_default_bool(settings, "reroute_audio", true);
+	obs_data_set_default_bool(settings, "reroute_audio", false);
 }
 
 static bool is_local_file_modified(obs_properties_t *props, obs_property_t *,
