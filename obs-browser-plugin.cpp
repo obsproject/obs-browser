10515adb45b9b5723f6b1d8b73491e98e1942d11
Matt Gajownik <matt@obsproject.com>
2020-08-24 16:10:29 +1000

    Add module description

--- obs-browser-plugin.cpp
+++ obs-browser-plugin.cpp
@@ -50,6 +50,10 @@
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("obs-browser", "en-US")
+MODULE_EXPORT const char *obs_module_description(void)
+{
+	return "CEF-based web browser source & panels";
+}
 
 using namespace std;
 using namespace json11;
