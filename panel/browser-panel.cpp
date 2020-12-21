8ed7220e6398ed44df12393e8df5061d0eca3ce9
Theodore Dubois <tblodt@icloud.com>
2020-12-21 14:25:51 -0800

    Handle the cookie directory not existing
    obs_get_abs_path_ptr on Mac uses realpath, which fails if the file
    doesn't exist.

--- panel/browser-panel.cpp
+++ panel/browser-panel.cpp
@@ -84,6 +84,8 @@ struct QCefCookieManagerInternal : QCefCookieManager {
 			throw "Browser thread not initialized";
 
 		BPtr<char> rpath = obs_module_config_path(storage_path.c_str());
+		if (os_mkdirs(rpath.Get()) != 0)
+			throw "Failed to create cookie diretory";
 		BPtr<char> path = os_get_abs_path_ptr(rpath.Get());
 
 #if CHROME_VERSION_BUILD < 3770
