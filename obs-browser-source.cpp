obsproject/obs-browser

commit 336fdf2224bd0b550c59119eece90af5e4db442d

Fix Windows key modifiers

obs-browser-source.cpp
@@ -305,22 +305,25 @@ void BrowserSource::SendFocus(bool focus)
 
 void BrowserSource::SendKeyClick(const struct obs_key_event *event, bool key_up)
 {
-	uint32_t modifiers = event->modifiers;
-	UNUSED_PARAMETER(modifiers);
 	std::string text = event->text;
 #ifdef __linux__
 	uint32_t native_vkey = KeyboardCodeFromXKeysym(event->native_vkey);
-#else
+	uint32_t modifiers = event->native_modifiers;
+#elif defined(_WIN32)
 	uint32_t native_vkey = event->native_vkey;
-#endif
+	uint32_t modifiers = event->modifiers;
+#else
 	uint32_t native_scancode = event->native_scancode;
-	uint32_t native_modifiers = event->native_modifiers;
+	UNUSED_PARAMETER(modifiers);
+#endif
 
 	ExecuteOnBrowser(
 		[=](CefRefPtr<CefBrowser> cefBrowser) {
 			CefKeyEvent e;
 			e.windows_key_code = native_vkey;
+#ifdef __APPLE__
 			e.native_key_code = native_scancode;
+#endif
 
 			e.type = key_up ? KEYEVENT_KEYUP : KEYEVENT_RAWKEYDOWN;
 
@@ -331,7 +334,7 @@ void BrowserSource::SendKeyClick(const struct obs_key_event *event, bool key_up)
 			}
 
 			//e.native_key_code = native_vkey;
-			e.modifiers = native_modifiers;
+			e.modifiers = modifiers;
 
 			cefBrowser->GetHost()->SendKeyEvent(e);
 			if (!text.empty() && !key_up) {
@@ -339,10 +342,11 @@ void BrowserSource::SendKeyClick(const struct obs_key_event *event, bool key_up)
 #ifdef __linux__
 				e.windows_key_code =
 					KeyboardCodeFromXKeysym(e.character);
-#else
+#elif defined(_WIN32)
 				e.windows_key_code = e.character;
-#endif
+#else
 				e.native_key_code = native_scancode;
+#endif
 				cefBrowser->GetHost()->SendKeyEvent(e);
 			}
 		},

commit 51bf1e810f7433dbf4d4237ad3fc9aff8d841169

Fix macOS build error introduced in #236
This does not fix modifiers on macOS, that seems to be a bigger task.

obs-browser-source.cpp
@@ -313,8 +313,9 @@ void BrowserSource::SendKeyClick(const struct obs_key_event *event, bool key_up)
 	uint32_t native_vkey = event->native_vkey;
 	uint32_t modifiers = event->modifiers;
 #else
+	uint32_t native_vkey = event->native_vkey;
 	uint32_t native_scancode = event->native_scancode;
-	UNUSED_PARAMETER(modifiers);
+	uint32_t modifiers = event->native_modifiers;
 #endif
 
 	ExecuteOnBrowser(