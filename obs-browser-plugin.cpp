1fc47fb7e644eb3ad88247371dc3a2e64926d642
V <v@anomalous.eu>
2021-06-10 18:36:22 +0200

    Set user_agent_product in CEF >=91

--- obs-browser-plugin.cpp
+++ obs-browser-plugin.cpp
@@ -298,7 +298,11 @@ static void BrowserInit(void)
 	prod_ver << std::to_string(obs_maj) << "." << std::to_string(obs_min)
 		 << "." << std::to_string(obs_pat);
 
+#if CHROME_VERSION_BUILD >= 4472
+	CefString(&settings.user_agent_product) = prod_ver.str();
+#else
 	CefString(&settings.product_version) = prod_ver.str();
+#endif
 
 #ifdef USE_QT_LOOP
 	settings.external_message_pump = true;
