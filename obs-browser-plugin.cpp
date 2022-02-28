obsproject/obs-browser

commit f93675c251859c213af0a27a4cf8a2fcfd93f33d

Print both runtime & compile version of CEF on startup
This will make it clearer when the libcef.dll is replaced or outdated.

obs-browser-plugin.cpp
@@ -697,7 +697,10 @@ static void check_hwaccel_support(void)
 bool obs_module_load(void)
 {
 	blog(LOG_INFO, "[obs-browser]: Version %s", OBS_BROWSER_VERSION_STRING);
-	blog(LOG_INFO, "[obs-browser]: CEF Version %s", CEF_VERSION);
+	blog(LOG_INFO,
+	     "[obs-browser]: CEF Version %i.%i.%i.%i (runtime), %s (compiled)",
+	     cef_version_info(4), cef_version_info(5), cef_version_info(6),
+	     cef_version_info(7), CEF_VERSION);
 #ifdef USE_QT_LOOP
 	qRegisterMetaType<MessageTask>("MessageTask");
 #endif

commit cbce71dd826cafb7c75cf99cbc7ce101ea7d03c4

Log CEF version *after* library is loaded on macOS
This fixes a crash on startup introduced in f93675c251859c213af0a27a4cf8a2fcfd93f33d

obs-browser-plugin.cpp
@@ -696,11 +696,6 @@ static void check_hwaccel_support(void)
 
 bool obs_module_load(void)
 {
-	blog(LOG_INFO, "[obs-browser]: Version %s", OBS_BROWSER_VERSION_STRING);
-	blog(LOG_INFO,
-	     "[obs-browser]: CEF Version %i.%i.%i.%i (runtime), %s (compiled)",
-	     cef_version_info(4), cef_version_info(5), cef_version_info(6),
-	     cef_version_info(7), CEF_VERSION);
 #ifdef USE_QT_LOOP
 	qRegisterMetaType<MessageTask>("MessageTask");
 #endif
@@ -719,6 +714,12 @@ bool obs_module_load(void)
 		return false;
 #endif
 #endif
+	blog(LOG_INFO, "[obs-browser]: Version %s", OBS_BROWSER_VERSION_STRING);
+	blog(LOG_INFO,
+	     "[obs-browser]: CEF Version %i.%i.%i.%i (runtime), %s (compiled)",
+	     cef_version_info(4), cef_version_info(5), cef_version_info(6),
+	     cef_version_info(7), CEF_VERSION);
+
 	RegisterBrowserSource();
 	obs_frontend_add_event_callback(handle_obs_frontend_event, nullptr);
 