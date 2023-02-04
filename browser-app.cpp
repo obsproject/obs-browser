b19b3a4fa30edd2a9d416549beaa1d3a6bade957
Matt Gajownik <matt@obsproject.com>
2023-02-04 19:53:47 +1100

    Use CefParseJSON instead of V8 context for JS responses
    Nlohmann's `dump()` uses a different method of escaping characters in
    strings than V8 does, resulting in `JSON.parse()` failing and the JS
    commands never returning a value.
    
    Instead, pass the Nlohmann `dump()` string directly into CEF's JSON
    parser, convert it into a CefV8Value that V8 will recognise, then
    forward the resulting value to the callback.
    
    Fixes #390

--- browser-app.cpp
+++ browser-app.cpp
@@ -243,6 +243,56 @@ void BrowserApp::SetDocumentVisibility(CefRefPtr<CefBrowser> browser,
 }
 #endif
 
+CefRefPtr<CefV8Value> CefValueToCefV8Value(CefRefPtr<CefValue> value)
+{
+	CefRefPtr<CefV8Value> result;
+	switch (value->GetType()) {
+	case VTYPE_INVALID:
+		result = CefV8Value::CreateNull();
+		break;
+	case VTYPE_NULL:
+		result = CefV8Value::CreateNull();
+		break;
+	case VTYPE_BOOL:
+		result = CefV8Value::CreateBool(value->GetBool());
+		break;
+	case VTYPE_INT:
+		result = CefV8Value::CreateInt(value->GetInt());
+		break;
+	case VTYPE_DOUBLE:
+		result = CefV8Value::CreateDouble(value->GetDouble());
+		break;
+	case VTYPE_STRING:
+		result = CefV8Value::CreateString(value->GetString());
+		break;
+	case VTYPE_BINARY:
+		result = CefV8Value::CreateNull();
+		break;
+	case VTYPE_DICTIONARY: {
+		result = CefV8Value::CreateObject(nullptr, nullptr);
+		CefRefPtr<CefDictionaryValue> dict = value->GetDictionary();
+		CefDictionaryValue::KeyList keys;
+		dict->GetKeys(keys);
+		for (unsigned int i = 0; i < keys.size(); i++) {
+			CefString key = keys[i];
+			result->SetValue(
+				key, CefValueToCefV8Value(dict->GetValue(key)),
+				V8_PROPERTY_ATTRIBUTE_NONE);
+		}
+	} break;
+	case VTYPE_LIST: {
+		CefRefPtr<CefListValue> list = value->GetList();
+		size_t size = list->GetSize();
+		result = CefV8Value::CreateArray((int)size);
+		for (int i = 0; i < size; i++) {
+			result->SetValue(
+				i, CefValueToCefV8Value(list->GetValue(i)));
+		}
+	} break;
+	}
+	return result;
+}
+
 bool BrowserApp::OnProcessMessageReceived(CefRefPtr<CefBrowser> browser,
 					  CefRefPtr<CefFrame> frame,
 					  CefProcessId source_process,
@@ -312,8 +362,6 @@ bool BrowserApp::OnProcessMessageReceived(CefRefPtr<CefBrowser> browser,
 	} else if (message->GetName() == "executeCallback") {
 		CefRefPtr<CefV8Context> context =
 			browser->GetMainFrame()->GetV8Context();
-		CefRefPtr<CefV8Value> retval;
-		CefRefPtr<CefV8Exception> exception;
 
 		context->Enter();
 
@@ -321,16 +369,13 @@ bool BrowserApp::OnProcessMessageReceived(CefRefPtr<CefBrowser> browser,
 		int callbackID = arguments->GetInt(0);
 		CefString jsonString = arguments->GetString(1);
 
-		std::string script;
-		script += "JSON.parse('";
-		script += arguments->GetString(1).ToString();
-		script += "');";
+		CefRefPtr<CefValue> json =
+			CefParseJSON(arguments->GetString(1).ToString(), {});
 
 		CefRefPtr<CefV8Value> callback = callbackMap[callbackID];
 		CefV8ValueList args;
 
-		context->Eval(script, browser->GetMainFrame()->GetURL(), 0,
-			      retval, exception);
+		CefRefPtr<CefV8Value> retval = CefValueToCefV8Value(json);
 
 		args.push_back(retval);
 
