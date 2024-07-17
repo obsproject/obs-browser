7022c48277b4ab2ec174df0f0ee9e12b0ed7e4d4
thirumalai-qcom <quic_tnagalin@quicinc.com>
2024-07-17 16:31:31 +0530

    Fix Guard DPI scaling for ARM64 in obs-browser

--- obs-browser-page/obs-browser-page-main.cpp
+++ obs-browser-page/obs-browser-page-main.cpp
@@ -79,8 +79,10 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	std::thread shutdown_check;
 
 	CefMainArgs mainArgs(nullptr);
+#if !defined(_M_ARM64)
 	if (!SetHighDPIv2Scaling())
 		CefEnableHighDPISupport();
+#endif
 
 	CefRefPtr<CefCommandLine> command_line =
 		CefCommandLine::CreateCommandLine();
--- obs-browser-plugin.cpp
+++ obs-browser-plugin.cpp
@@ -750,7 +750,9 @@ bool obs_module_load(void)
 
 #ifdef _WIN32
 	/* CefEnableHighDPISupport doesn't do anything on OS other than Windows. Would also crash macOS at this point as CEF is not directly linked */
+#if !defined(_M_ARM64)
 	CefEnableHighDPISupport();
+#endif
 	EnumAdapterCount();
 #else
 #if defined(__APPLE__) && !defined(ENABLE_BROWSER_LEGACY)
