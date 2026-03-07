441ee7b75b1090943e1008981d2503aee3a5780c
Hoshino Lina <lina@lina.yt>
2026-03-07 20:42:22 +0900

    Update to C++20
    Current CEF requires compiling as C++20.

--- cmake/os-linux.cmake
+++ cmake/os-linux.cmake
@@ -19,6 +19,7 @@ target_sources(
 target_include_directories(browser-helper PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/deps"
                                                   "${CMAKE_CURRENT_SOURCE_DIR}/obs-browser-page")
 
+target_compile_features(browser-helper PRIVATE cxx_std_20)
 target_link_libraries(browser-helper PRIVATE CEF::Wrapper CEF::Library)
 
 target_sources(obs-browser PRIVATE deps/ip-string-posix.cpp)
--- cmake/os-windows.cmake
+++ cmake/os-windows.cmake
@@ -18,6 +18,7 @@ target_include_directories(obs-browser-helper PRIVATE "${CMAKE_CURRENT_SOURCE_DI
 
 target_compile_options(obs-browser-helper PRIVATE $<IF:$<CONFIG:DEBUG>,/MTd,/MT>)
 target_compile_definitions(obs-browser-helper PRIVATE ENABLE_BROWSER_SHARED_TEXTURE)
+target_compile_features(obs-browser-helper PRIVATE cxx_std_20)
 
 target_link_libraries(obs-browser-helper PRIVATE CEF::Wrapper CEF::Library nlohmann_json::nlohmann_json)
 target_link_options(obs-browser-helper PRIVATE /IGNORE:4099 /SUBSYSTEM:WINDOWS)
